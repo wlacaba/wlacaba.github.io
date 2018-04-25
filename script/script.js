9d3442eeb8ec63e39be8577287b166c4940127fe
Warren Lacaba <lacaba.w@gmail.com>
2018-04-25 09:35:49 -0500

    Updated projects section. Fixed some code indentations.

--- script/script.js
+++ script/script.js
@@ -1,44 +1,44 @@
 function check(media) {
-    if (media.matches) {
-        showSlide(1, 0);
-        showSlide(1, 1);
-    }
-    else {
-        showNormal("analog-image");
-        showNormal("poly-image");
-    }
+  if (media.matches) {
+    showSlide(1, 0);
+    showSlide(1, 1);
+  }
+  else {
+    showNormal("analog-image");
+    showNormal("poly-image");
+  }
 }
 
 function traverseSlide(n, classNum) {
-    showSlide(indexes[classNum] += n, classNum);
+  showSlide(indexes[classNum] += n, classNum);
 }
 
 function showSlide(n, classNum) {
-    var i;
-    var images = document.getElementsByClassName(classes[classNum]);
+  var i;
+  var images = document.getElementsByClassName(classes[classNum]);
 
-    if (n > images.length) {
-        indexes[classNum] = 1;
-    }
+  if (n > images.length) {
+    indexes[classNum] = 1;
+  }
 
-    if (n < 1) {
-        indexes[classNum] = images.length;
-    }
+  if (n < 1) {
+    indexes[classNum] = images.length;
+  }
 
-    for (i = 0; i < images.length; i++) {
-        images[i].style.display = "none";
-    }
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "none";
+  }
 
-    images[indexes[classNum] - 1].style.display = "block";
+  images[indexes[classNum] - 1].style.display = "block";
 }
 
 function showNormal(className) {
-    var i;
-    var images = document.getElementsByClassName(className);
+  var i;
+  var images = document.getElementsByClassName(className);
 
-    for (i = 0; i < images.length; i++) {
-        images[i].style.display = "inline-block";
-    }
+  for (i = 0; i < images.length; i++) {
+    images[i].style.display = "inline-block";
+  }
 }
 
 var indexes = [1, 1];
@@ -50,17 +50,17 @@ check(mediaQuery);
 mediaQuery.addListener(check);
 
 document.getElementById('analog-prev').onclick = function() {
-    traverseSlide(-1, 0);
+  traverseSlide(-1, 0);
 };
 
 document.getElementById('analog-next').onclick = function() {
-    traverseSlide(1, 0);
+  traverseSlide(1, 0);
 }
 
 document.getElementById('poly-prev').onclick = function() {
-    traverseSlide(-1, 1);
+  traverseSlide(-1, 1);
 };
 
 document.getElementById('poly-next').onclick = function() {
-    traverseSlide(1, 1);
+  traverseSlide(1, 1);
 }
